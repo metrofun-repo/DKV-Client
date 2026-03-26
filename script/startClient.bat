8b89955255cb11dd9b4005283397fd04b9fc1cdc
Vladyslav M. <25565850+metrofun-repo@users.noreply.github.com>
2026-03-26 03:27:53 +0200

    feature(json): update client API to new JSON request/response format

--- script/startClient.bat
+++ script/startClient.bat
@@ -1,3 +1,6 @@
 cd ..\build\Debug\
 
 start "" dkv_client.exe --host 127.0.0.1 --port 8080
+rem start "" dkv_client.exe --host 127.0.0.1 --port 8081
+rem start "" dkv_client.exe --host 127.0.0.1 --port 8082
+rem start "" dkv_client.exe --host 127.0.0.1 --port 8083
--- src/api/KeyValueApi.h
+++ src/api/KeyValueApi.h
@@ -3,24 +3,38 @@
 #include <string>
 #include <optional>
 
-class HttpClient;
-struct HttpResponse;
+#include <nlohmann/json.hpp>
+
+#include "core/types/BasicResult.h"
+#include "http/HttpClient.h"
+#include "http/HttpResponse.h"
 
 class KeyValueApi
 {
-    HttpClient& client;
+    using StorageKey       = std::string;
+    using StorageValue     = std::string;
 
-    bool validateKey(const std::string& key, const char* func) const;
-    bool validateValue(const std::string& value, const char* func) const;
-    void handleError(const HttpResponse& response, const char* func) const;
-    std::optional<std::string> handleErrorAndReturnEmpty(const HttpResponse& response, const char* func) const;
+    using Json             = nlohmann::json;
+    using ValidationResult = BasicResult<void>;
+    using JsonResult       = BasicResult<Json>;
+    using ApiCallResult    = BasicResult<std::optional<StorageValue>>;
+    using JsonPrintResult  = BasicResult<void>;
+    using JsonPrintResult  = BasicResult<void>;
 
-    std::string makeURL(const std::string& endpoint, const std::string& key) const;
-
+    
 public:
     KeyValueApi(HttpClient& cl);
 
-    void set(const std::string& key, const std::string& value);
-    std::optional<std::string> get(const std::string& key);
-    void remove(const std::string& key);
+    ApiCallResult set(const StorageKey& key, const StorageValue& value);
+    ApiCallResult get(const StorageKey& key);
+    ApiCallResult remove(const StorageKey& key);
+private:
+    std::string makeURL(const std::string& endpoint, const std::string& key) const;
+    ValidationResult validateKey(const StorageKey& key, const char* func) const;
+    ValidationResult validateValue(const StorageValue& value, const char* func) const;
+    JsonResult parseJson(const HttpResponse& response, const char* func) const;
+
+    void printJsonPayload(const Json& json, const char* func) const;
+
+    HttpClient& client;
 };
